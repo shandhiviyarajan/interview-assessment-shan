e730752eb71564d8364964a3058a61462f6053ae
Shan Dhiviyarajan <prashasoft@gmail.com>
2023-05-28 01:31:25 +0530

    Profile, cart, checkout pages

--- iApp/components/screens/cart/views/cart.js
+++ iApp/components/screens/cart/views/cart.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-import { Text, Wrapper } from 'components/atoms';
+import { Box, Button, Text, Wrapper } from 'components/atoms';
+import { TAB_NAVIGATIONS } from 'core/constants/routes';
+import { useAppTheme } from 'core/theme';
 
 import { useRoute } from '@react-navigation/native';
-const CartPage = () => {
+const CartPage = ({ navigation }) => {
+  const checkOut = () => {
+    navigation.navigate(TAB_NAVIGATIONS.CHECKOUT, {});
+  };
   const route = useRoute();
+  const { Colors, Gutters } = useAppTheme();
   return (
-    <Wrapper>
-      <Text>{JSON.stringify(route)}</Text>
+    <Wrapper paddingH={Gutters.xl}>
+      <Box flex={5}>
+        <Text>{JSON.stringify(route)}</Text>
+        <Text>{JSON.stringify(route)}</Text>
+      </Box>
+      <Box justifyContent="flex-end" height={44} pb={Gutters.md}>
+        <Button width="100%" onPress={checkOut}>
+          <Text color={Colors.Light}>Checkout</Text>
+        </Button>
+      </Box>
     </Wrapper>
   );
 };
--- iApp/components/screens/profile/views/profile.js
+++ iApp/components/screens/profile/views/profile.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-import { Text, View } from 'react-native';
+import { Box, Button, Text, Wrapper } from 'components/atoms';
+import { useAppTheme } from 'core/theme';
 const ProfilePage = () => {
+  const { Colors, Gutters } = useAppTheme();
+  const logout = () => {};
   return (
-    <View>
-      <Text>Profile Page</Text>
-    </View>
+    <Wrapper paddingH={Gutters.xl}>
+      <Box flex={3}>
+        <Text>Profile information</Text>
+      </Box>
+
+      <Box justifyContent="flex-end" height={44} pb={Gutters.md}>
+        <Button width="100%" onPress={logout}>
+          <Text color={Colors.Light}>Logout</Text>
+        </Button>
+      </Box>
+    </Wrapper>
   );
 };
 
