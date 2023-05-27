76e0bdc97ca269335712023504c4d23338235eaf
Shan Dhiviyarajan <prashasoft@gmail.com>
2023-05-27 22:59:14 +0530

    wrapper

--- iApp/components/screens/products/views/product.js
+++ iApp/components/screens/products/views/product.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
+import { Wrapper } from 'components/atoms';
 import { TAB_NAVIGATIONS } from 'core/constants/routes';
 import { LinkNavigate } from 'core/utils/helper';
-import {
-  Text,
-  View
-} from 'react-native';
+import { Text } from 'react-native';
 
 import { Link } from '@react-navigation/native';
 const ProductsPage = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Wrapper justifyContent="center" alignItems="center">
       <Text>Products Page</Text>
       <Link to={LinkNavigate(TAB_NAVIGATIONS.SINGLE_PRODUCT, {})}>Single Product</Link>
-    </View>
+    </Wrapper>
   );
 };
 
